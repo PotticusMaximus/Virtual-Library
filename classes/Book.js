9f2146a721857a3052bb52e327868de6b9ce928d
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-10-24 14:29:52 +0100

    Books work

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,21 @@
 // import the Media class:
-
+const Media = require("/Users/mpi976/Documents/Multiverse/Virtual-Library/main");
 // create your Book class:
+class Book extends Media {
+  constructor(title, genre, year, author, numPages, rating) {
+    super(title, genre, year);
+    this.author = author;
+    this.numPages = numPages;
+    this.rating = rating;
+  }
+  summary() {
+    return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+  static highestRating(book) {
+    const sorted = book.sort((a, b) => a.rating - b.rating);
+    return sorted[0].title;
+  }
+}
 
 // don't change below
 module.exports = Book;
--- classes/Media.js
+++ classes/Media.js
@@ -5,6 +5,7 @@ class Media {
     this.title = title;
     this.genre = genre;
     this.year = year;
+    Media.totalMediaCount++;
   }
   summary() {
     return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre} `;
