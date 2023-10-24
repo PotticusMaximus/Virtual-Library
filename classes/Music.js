c2b388fcbb5c715b38ce8c57c4113e08c6421052
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-10-24 14:41:08 +0100

    Music work

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,20 @@
 // import the Media class:
-
+const Media = require("/Users/mpi976/Documents/Multiverse/Virtual-Library/main");
 // create your Music class:
+class Music extends Media {
+  constructor(title, genre, year, artist, length) {
+    super(title, genre, year);
+    this.artist = artist;
+    this.length = length;
+  }
+  summary() {
+    return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length}`;
+  }
 
+  static shortestAlbum(album) {
+    const sorted = album.sort((a, b) => a.length - b.length);
+    return sorted[album.length - 1].title;
+  }
+}
 // don't change below
 module.exports = Music;
