e8170f04c4f4b58eff7f62dfbf3e067e84d72426
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-10-24 14:35:46 +0100

    Movie work

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,19 @@
 // import the Media class:
-
+const Media = require("/Users/mpi976/Documents/Multiverse/Virtual-Library/main");
 // create your Movie class:
-
+class Movie extends Media {
+  constructor(title, genre, year, director, duration) {
+    super(title, genre, year);
+    this.director = director;
+    this.duration = duration;
+  }
+  summary() {
+    return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Duration: ${this.duration}, Rating: ${this.rating}`;
+  }
+  static longestMovie(movie) {
+    const sorted = movie.sort((a, b) => a.duration - b.duration);
+    return sorted[0].title;
+  }
+}
 // don't change below
 module.exports = Movie;
